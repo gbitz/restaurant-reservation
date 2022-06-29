71b79681ff6749ff9bd461d24091235b93253475
Grant Bitzer <gbitzer86@gmail.com>
2022-06-29 15:31:23 -0500

    added check to hide table from list if already occupied

--- front-end/src/layout/reservations/SeatForm.js
+++ front-end/src/layout/reservations/SeatForm.js
@@ -3,11 +3,13 @@ import React from "react"
 function SeatForm({tables, changeHandler, cancelHandler, submitHandler, form}){
 
     const tablesMenu = tables.map((table)=> {
-        return(
-            <option key={table.table_id} value={table.table_id}>
-                {table.table_name} - {table.capacity}
-            </option>
-        )
+        if (!table.reservation_id) {
+            return(
+                <option key={table.table_id} value={table.table_id}>
+                    {table.table_name} - {table.capacity}
+                </option>
+            )
+        }
     })
 
     return (

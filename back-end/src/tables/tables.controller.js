32fe67f44c41050c04b646bce8fb3a8ac6687a88
Grant Bitzer <gbitzer86@gmail.com>
2022-06-29 20:57:48 -0500

    updated backend api controls

--- back-end/src/tables/tables.controller.js
+++ back-end/src/tables/tables.controller.js
@@ -15,11 +15,11 @@ function bodyDataHas(propertyName) {
 
 function checkValidCapacity(req, res, next) {
     const {capacity} = req.body.data;
-
-    if(capacity > 0) {
+    const testNumber = Number(capacity)
+    if(testNumber >= 1 && typeof(capacity) === "number") {
         return next();
     }
-    next({status:400, message:`Capacity must be at least 1`})
+    next({status:400, message:`capacity must be at least 1`})
 }
 
 function checkValidTableName(req, res,next) {
@@ -27,7 +27,7 @@ function checkValidTableName(req, res,next) {
     if (table_name.length >= 2) {
         return next()
     } 
-    next({status:400, message:`Table Name must be at least 2 characters long`})
+    next({status:400, message:`table_name must be at least 2 characters long`})
 
 }
 
@@ -69,18 +69,33 @@ async function checkTableExists(req, res, next) {
         res.locals.table = table;
         return next()
     } 
-    next({status:404, message:`Table does not exist`})
+    next({status:404, message:`Table ${table_id} does not exist`})
 }
 
 async function checkReservationExists(req,res,next) {
     const {reservation_id} =  req.body.data;
-    const reservation = await reservationService.read(reservation_id);
+    const reservation = await reservationService.read(Number(reservation_id));
     if (reservation) {
         res.locals.reservation = reservation;
         return next();
     }
-    next({status:404, message:`Reservation does not exist`})
+    next({status:404, message:`${reservation_id} does not exist`})
+}
 
+async function checkTableSize(req,res,next) {
+    const {table, reservation} = res.locals;
+    if(table.capacity >= reservation.people) {
+        return next();
+    }
+    next({status:400, message:"insufficient table capacity"})
+} 
+
+async function checkIfOccupied(req,res,next) {
+    const {table} = res.locals;
+    if (!table.reservation_id) {
+        return next();
+    }
+    next({status:400, message:"table is occupied"})
 }
 
 
@@ -94,8 +109,11 @@ module.exports ={
         asyncErrorBoundary(create)
     ],
     update: [
+        bodyDataHas("reservation_id"),
         checkTableExists,
         checkReservationExists,
+        checkTableSize,
+        checkIfOccupied,
         asyncErrorBoundary(update),
-    ]
+    ],
 }

7cafb188fc66331384a9b86c0854646027fc1774
Grant Bitzer <gbitzer86@gmail.com>
2022-06-30 09:02:59 -0500

    added button to finish reservation that has been seated at table

--- front-end/src/layout/tables/TableView.js
+++ front-end/src/layout/tables/TableView.js
@@ -1,6 +1,23 @@
 import React from "react";
-
+import ErrorAlert from "../ErrorAlert";
 function TableView({tables}) {
+
+    async function handleFinish(table_id) {
+        const abortController = new AbortController();
+
+        if (window.confirm("Is this table ready to seat new guests? This cannot be undone.")) {
+            try {
+                await finishReservation(table);
+            } catch (error) {
+                if (error.name !== "AbortError") {setError(error)}
+            }
+            return () => {
+                abortController.abort();
+              };
+        }
+    }
+    
+
     const tablesList = tables.map((table, index) => {
         return (
             <tr key={table.table_id}>
@@ -10,6 +27,10 @@ function TableView({tables}) {
                 {table.reservation_id
                     ? <td data-table-id-status={table.table_id}>Occupied</td>
                     : <td data-table-id-status={table.table_id}>Free</td>
+                }
+                {table.reservation_id
+                    ? <td><button data-table-id-status={table.table_id}  className="btn btn-danger" onClick={()=>handleFinish(table.table_id)}>Finish</button></td>
+                    : <td>Add Reservation</td>
                 }
             </tr>
         )
@@ -24,6 +45,7 @@ function TableView({tables}) {
                         <th scope="col">Name</th>
                         <th scope="col">Capacity</th>
                         <th scope="col">Status</th>
+                        <th scope="col">Remove</th>
                     </tr>
                 </thead>
 

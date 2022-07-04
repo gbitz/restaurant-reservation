f57f70c3898fcb9066987ed5daafe0bcce063b3c
Grant Bitzer <gbitzer86@gmail.com>
2022-07-04 08:34:07 -0500

    added methodNotAllowed error to backend routers

--- back-end/src/reservations/reservations.router.js
+++ back-end/src/reservations/reservations.router.js
@@ -1,3 +1,4 @@
+import methodNotAllowed from "../errors/methodNotAllowed"
 /**
  * Defines the router for reservation resources.
  *
@@ -7,8 +8,8 @@
 const router = require("express").Router();
 const controller = require("./reservations.controller");
 
-router.route("/").get(controller.list).post(controller.create)
-router.route("/:reservation_id").get(controller.read).put(controller.update)
-router.route("/:reservation_id/status").put(controller.updateStatus)
+router.route("/").get(controller.list).post(controller.create).all(methodNotAllowed)
+router.route("/:reservation_id").get(controller.read).put(controller.update).all(methodNotAllowed)
+router.route("/:reservation_id/status").put(controller.updateStatus).all(methodNotAllowed)
 
 module.exports = router;
--- back-end/src/tables/tables.router.js
+++ back-end/src/tables/tables.router.js
@@ -1,3 +1,5 @@
+import methodNotAllowed from "../errors/methodNotAllowed"
+
 /**
  * Defines the router for table resources.
  *
@@ -10,9 +12,11 @@ const controller = require("./tables.controller");
 router.route("/")
     .get(controller.list)
     .post(controller.create)
+    .all(methodNotAllowed)
 
 router.route("/:table_id/seat")
     .put(controller.update)
     .delete(controller.finishReservation)
-
+    .all(methodNotAllowed)
+    
 module.exports = router;
